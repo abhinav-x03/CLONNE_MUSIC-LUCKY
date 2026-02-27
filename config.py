86124baa38637040662e31a299e8d126583a2d7d
Abhinav <bpushpa973@gmail.com>
2026-02-27 07:55:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ ASSUSERNAME = getenv("ASSUSERNAME" , "")
 # ---------------------------------------------------------
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## 
-YT_API_KEY = getenv("YT_API_KEY" , 'xbit_CD0721D6CTWKQEYHZ1BGEI') 
+YT_API_KEY = getenv("YT_API_KEY" , 'xbit_yjb819gMTo3xPm09-Y0vHA9hcgGIShPj') 
 COOKIES_URL=getenv("COOKIES_URL" , "https://gist.githubusercontent.com/sparrow9616/f29fc6588086a3c72d92dd9c03773350/raw/4229f3f4aab4a6693fc0794d136d30f54d67ae85/gistfile1.txt")
 
 #---------------------------------------------------------------
